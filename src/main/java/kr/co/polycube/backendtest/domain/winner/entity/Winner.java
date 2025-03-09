3e90dd285cd25e9eecbd2f27de45abaa06e99181
MoonMinHyuk1 <moon981018@gmail.com>
2025-03-09 22:40:01 +0900

    feat: winner 도메인 설계

--- src/main/java/kr/co/polycube/backendtest/domain/winner/entity/Winner.java
+++ src/main/java/kr/co/polycube/backendtest/domain/winner/entity/Winner.java
@@ -1,2 +1,33 @@
-package kr.co.polycube.backendtest.domain.winner.entity;public class Winner {
+package kr.co.polycube.backendtest.domain.winner.entity;
+
+import jakarta.persistence.*;
+import kr.co.polycube.backendtest.domain.lotto.entity.Lotto;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "winner")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Winner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "rank", nullable = false)
+    private Integer rank;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lotto_id", nullable = false)
+    private Lotto lotto;
+
+    @Builder
+    public Winner(Integer rank, Lotto lotto) {
+        this.rank = rank;
+        this.lotto = lotto;
+    }
 }
--- src/main/java/kr/co/polycube/backendtest/domain/winner/repository/WinnerRepository.java
+++ src/main/java/kr/co/polycube/backendtest/domain/winner/repository/WinnerRepository.java
@@ -1,2 +1,9 @@
-package kr.co.polycube.backendtest.domain.winner.repository;public interface WinnerRepository {
+package kr.co.polycube.backendtest.domain.winner.repository;
+
+import kr.co.polycube.backendtest.domain.winner.entity.Winner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WinnerRepository extends JpaRepository<Winner, Long> {
 }
